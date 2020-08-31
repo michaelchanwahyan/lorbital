dca58ad9e6e55eb578ee8286ce95a93133961216
aip <snorlax>
2020-08-31 11:49:50 +0800

    [bugfix] fix docker image name to lorbital

--- rundocker.sh
+++ rundocker.sh
@@ -11,5 +11,5 @@ docker run -dt \
            -e DISPLAY=$DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
            -v /data:/data \
-           loam-orbital:latest \
+           lorbital:latest \
            systemd
